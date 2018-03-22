3ad60e5ede8ac2ad3e4fb2a86a6435b501830ef9
Onuchukwu Chika <chuzksy@gmail.com>
2018-03-22 04:17:51 +0100

    Complete generting card list

--- client/src/components/EventCardList.jsx
+++ client/src/components/EventCardList.jsx
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import EventCards from './EventCards';
+
+const Events = props => {
+  const { events } = props;
+  return <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>{events.map(e => <EventCards key={e.id} event={e} />)}</div>;
+};
 
 class EventCardList extends Component {
   render() {
     const { loading, message, events } = this.props.events;
-    return <div style={{ marginTop: '30px' }}>{loading ? <div className="loader-big" /> : <div />}</div>;
+    return (
+      <div className="container">
+        <h1 class="header-section row-width">Up Coming events</h1>
+        <div style={{ margin: '30px 0' }}>
+          {loading ? <div className="loader-big" /> : message ? <div>{message}</div> : <Events events={events} />}
+        </div>
+      </div>
+    );
   }
 }
 
--- client/src/components/eventCards.jsx
+++ client/src/components/eventCards.jsx
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
+const moment = require('moment');
 
 class EventCards extends Component {
   render() {
+    const { name, purpose, location, eventDate } = this.props.event;
     return (
       <div className="event-card">
-        <h4>Events Name here</h4>
+        <h4>{name}</h4>
         <div>
-          <div>Purpose</div>
-          <div>Location</div>
-          <div>Date</div>
+          <div>{purpose}</div>
+          <div>{location}</div>
+          <div>{moment(eventDate).format('MMMM, Do YYYY')}</div>
           <p>MORE DETAILS</p>
         </div>
       </div>
