178512653d95f2f18fc61997f1b6f0607bf5b375
Onuchukwu Chika <chuzksy@gmail.com>
2018-03-23 10:38:05 +0100

    Add reducers to handle the fetching events by userId

--- client/src/reducer/eventReducer.js
+++ client/src/reducer/eventReducer.js
@@ -13,6 +13,12 @@ const eventReducer = (state = initialState, action) => {
       return { ...state, loading: false, events: action.payload, message: null };
     case types.FETCH_EVENT_FAILURE:
       return { ...state, loading: false, events: [], message: action.payload };
+    case types.FETCHING_EVENT_BY_ID:
+      return { ...state, loading: true, message: null };
+    case types.FETCH_EVENT_BY_ID_FAILURE:
+      return { ...state, loading: false, message: action.payload };
+    case types.FETCH_EVENT_BY_ID:
+      return { ...state, loading: false, message: null, events: action.payload };
     default:
       return state;
   }
