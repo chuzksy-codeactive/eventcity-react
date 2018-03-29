1feb5896a1ef9ac71fe6f58ca2a6fef3f96da73d
Onuchukwu Chika <chuzksy@gmail.com>
2018-03-29 18:25:03 +0100

    Add datepicker component to the event modal

--- client/src/components/EditEventModal.jsx
+++ client/src/components/EditEventModal.jsx
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 import { load } from '../reducer/loadCenter';
+import DayPicker from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
+
+const moment = require('moment');
 
 const renderFacilities = ({ label, input, meta: { touched, error, invalid } }) => (
   <div className={`form-group ${touched && invalid ? 'has-error' : ''}`}>
@@ -43,6 +47,20 @@ const renderField = ({ input, label, type, meta: { touched, error, invalid } }) 
 );
 
 class EditEventModal extends Component {
+  state = {
+    selectedDay: undefined,
+    message: null
+  };
+  handleDayClick = (day, { selected, disabled }) => {
+    if (disabled) {
+      return;
+    }
+    if (selected) {
+      this.setState({ selectedDay: undefined });
+      return;
+    }
+    this.setState({ selectedDay: day, message: null });
+  };
   componentDidUpdate() {
     if (this.props.event) {
       const { name, purpose, note } = this.props.event;
@@ -58,6 +76,8 @@ class EditEventModal extends Component {
 
   render() {
     const { handleSubmit } = this.props;
+    const { selectedDay } = this.state;
+    const { eventDate } = this.props.event;
     return (
       <div>
         <div className="modal-overlay" id="modal-overlay" ref={this.props.modal_overlay}>
@@ -82,6 +102,9 @@ class EditEventModal extends Component {
                       placeholder="e.g wedding, AGM, birthday, meetup"
                     />
                     <Field name="note" component={renderFacilities} label="Short note" />
+                    {eventDate && (
+                      <div style={{ margin: '10px 0', color: '#343A40' }}>{`Event date: ${moment(eventDate).format('MMMM Do YYYY')}`}</div>
+                    )}
                     <div className="modal-footer">
                       <button className="close-button btn btn-danger btn-sm" id="close-button" type="submit">
                         submit
@@ -91,6 +114,19 @@ class EditEventModal extends Component {
                       </button>
                     </div>
                   </form>
+                </div>
+                <div className="col-6">
+                  <div style={{ fontSize: '18px', fontWeight: 'bold', color: '#555' }}>Change date</div>
+                  <div>
+                    <DayPicker onDayClick={this.handleDayClick} selectedDays={this.state.selectedDay} disabledDays={{ before: new Date() }} />
+                  </div>
+                  {selectedDay ? (
+                    <div style={{ marginBottom: '10px', textAlign: 'center', color: '#78B0F6' }}>
+                      Date selected: {selectedDay.toLocaleDateString()}
+                    </div>
+                  ) : (
+                    <div />
+                  )}
                 </div>
               </div>
             </div>
